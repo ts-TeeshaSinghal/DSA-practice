a925c16f7528cd8a9b7a1c01dca570d2e4a32fc4
ts-TeeshSinghal <singhalteesha7@gmail.com>
2024-08-04 10:27:52 +0530

    move hyphen

--- Important/MoveHyphen.java
+++ Important/MoveHyphen.java
@@ -5,11 +5,26 @@ public class MoveHyphen {
 
     public static String moveToFirstHyphen(String s) {
 
-        return null;
+        if (s.length() == 0) {
+            return null;
+        }
+
+        StringBuilder hyphens = new StringBuilder();
+        StringBuilder alaphabet = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '-') {
+                hyphens.append(s.charAt(i));
+            } else {
+                alaphabet.append(s.charAt(i));
+            }
+        }
+        return hyphens.append(alaphabet).toString();
+
     }
 
     public static void main(String[] args) {
-        String s = "hello-world";
-        moveToFirstHyphen(s);
+        String s = "Hello-world";
+        System.out.println(moveToFirstHyphen(s));
     }
 }
